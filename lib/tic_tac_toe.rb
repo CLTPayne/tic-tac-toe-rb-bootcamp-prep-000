0c831c38290c620da045d260a951836e484de14a
Charlotte Payne <cltpayne@gmail.com>
2018-04-02 10:38:51 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,1 +1,100 @@
+WIN_COMBINATIONS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6]
+  ]
+
+def display_board (board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index (input)
+  input.to_i - 1
+end
+
+def move (board, index, token)
+  board[index] = token
+end
+
+def position_taken? (board, index)
+  taken = nil
+  if (board[index] ==  " " || board[index] == "" || board[index] == nil)
+    taken = false
+   else
+     taken = true
+  end
+   taken
+end
+
+def valid_move? (board, index)
+  index.between?(0,8) && !position_taken?(board, index)
+end
+
+def turn(board)
+  puts "Please enter 1-9:"
+  input = gets.chomp
+  token = "X" || "O"
+  index = input_to_index(input)
+  if valid_move?(board, index)
+    move(board, index, token)
+    display_board(board)
+  else
+    turn(board)
+  end
+end
+
+def turn_count (board)
+  board.count {|token| token == "X" || token == "O"}
+end
+
+def current_player (board)
+  turn_count(board) % 2 ? "X" : "O"
+end
+
+def won? (board)
+  WIN_COMBINATIONS.detect do |combo|
+    board[combo[0]] == board[combo[1]] &&
+    board[combo[1]] == board[combo[2]] &&
+    position_taken?(board, combo[0])
+  end
+end 
+
+def full? (board)
+  board.all?{|token| token == "X" || token == "O"}
+end
+
+def draw? (board)
+  return true if !won?(board) && full?(board)  
+end
+
+def over? (board)
+  return true if won?(board) || full?(board) || draw?(board)
+end
+
+def winner (board)
+  if winning_combo = won?(board)
+    board[winning_combo.first]
+  end 
+end
+
+def play (board)
+  while !over?(board)
+  turn(board)
+  end
+  if won?(board)
+    puts "Congratulations #{winner(board)}!"
+  elsif draw?(board)
+    puts "Cat's Game!"
+  end
+end
+
 
