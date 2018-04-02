20bb0ec2a37fc86f097d873f7055d8240d1462fb
Charlotte Payne <cltpayne@gmail.com>
2018-04-02 10:39:04 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -41,7 +41,7 @@ end
 
 def turn(board)
   puts "Please enter 1-9:"
-  input = gets.chomp
+  input = gets.strip
   token = "X" || "O"
   index = input_to_index(input)
   if valid_move?(board, index)
